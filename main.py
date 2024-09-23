5b859d14094f7b7d10273d14a3883b302cce2cfe
AnAn <2915947652@qq.com>
2024-09-23 23:21:46 +0800

    update: 修改了xcx

---
+++ main.py
@@ -0,0 +1,28 @@
+# ctrl + k
+
+# import sdk
+# import 第三方库
+
+# 红色        不是要提交的内容
+# 绿色        将要提交的内容
+# 白色        远程与本地相同
+
+
+# git init   share on gitee/github
+# git add .   提交谁  所有文件/文件夹
+# git commit  说明
+
+
+
+# ctrl + k 打开页面 选中要提交的内容 设置 --》 提交信息 --》
+# commit 提交  QQ输入框  -->对方未收到
+# push   发送按钮
+
+
+# 用户名+  邮箱
+#  git config user.name "AnAn"
+#  git config user.email "2915947652@qq.com"
+
+
+if __name__ == '__main__':
+    print("asa")
